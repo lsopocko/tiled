1c940be4b10c5960afd0bf0d0d84df253d44afa2
Łukasz Sopoćko <lukasz.sopocko@gmail.com>
2018-06-30 18:03:53 +0200

    Cleanup and tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display test message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to tiled!');
+    expect(page.getParagraphText()).toEqual('Tiled');
   });
 });
